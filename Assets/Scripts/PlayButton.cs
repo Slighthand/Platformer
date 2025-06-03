c7585539f73e22f24918d0ca3430dd68848b3311
anqiey <144811036+anqiey@users.noreply.github.com>
2025-06-02 22:26:37 -0400

    Added a working play button from the main menu, which takes the player to the gameplay screen.

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static void StartGame(string scene)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}

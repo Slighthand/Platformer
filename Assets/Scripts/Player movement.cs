af27188b009883b179f21ddb176647304031e48a
anqiey <144811036+anqiey@users.noreply.github.com>
2025-06-05 23:00:24 -0400

    Created a potential placeholder menu for the victory screen

--- Assets/Scripts/Player movement.cs
+++ Assets/Scripts/Player movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 10;
 
     // Start is called before the first frame update
     void Start()

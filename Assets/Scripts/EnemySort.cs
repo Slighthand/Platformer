1f1b2f2b33c5395aa53a5b2bf987e469974e712e
anqiey <144811036+anqiey@users.noreply.github.com>
2025-06-08 22:34:44 -0400

    Started a script to sort enemies by distance during the gameplay.

---
+++ Assets/Scripts/EnemySort.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySort : MonoBehaviour
+{
+    int first = 1;
+    int second = 2;
+    int third = 3;
+    int fourth = 4;
+    // sort enemies by distance
+    public static void BubbleSort(int[] numbers)
+    {
+        for (int j = 0; j < numbers.Length; j++)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                Debug.Log(numbers[i] + " " + numbers[i + 1]);
+                if (numbers[i] > numbers[i + 1])
+                {
+                    int tmp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = tmp;
+                }
+            }
+        }
+
+        foreach (int n in numbers)
+        {
+            Debug.Log(n);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[] {6,4,3,5,2,1};
+        BubbleSort(nums);
+    }
+
+}

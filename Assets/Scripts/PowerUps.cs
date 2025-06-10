7bfc5fcfcb19f42f7d08f8aca68f4cdb0b16a936
anqiey <144811036+anqiey@users.noreply.github.com>
2025-06-10 10:20:50 -0400

    Fixing merge conflict

--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -1,20 +1,11 @@
 
 
-<<<<<<< HEAD
-public class PowerUps 
-{
-    public string Name;
-    public int Cost;
-    // public Sprite Icon;
-    public int Quantity;
-=======
 //public class PowerUps 
 //{
 //    public string Name;
 //    public int Cost;
 //    public Sprite Icon;
 //    public int Quantity;
->>>>>>> 9d823b2cdf66c1a130b4971643b6d6a1702ba3f6
 
 //    public PowerUps(string name, int cost, Sprite icon)
 //    {
@@ -24,10 +15,5 @@ public class PowerUps
 //        Quantity = 0;
 //    }
 
-<<<<<<< HEAD
-//     public virtual void ApplyEffect(GameObject player) { }
-// }
-=======
 //    public virtual void ApplyEffect(GameObject player) { }
 //}
->>>>>>> 9d823b2cdf66c1a130b4971643b6d6a1702ba3f6

5c94a3fb34cec0f41365585fb0f83c02991f78fb
Tarlan <158780016+ajidf@users.noreply.github.com>
2025-06-09 21:29:11 -0400

    Created the Scripts for the shopStore
    There might be some errors due to me not putting the images which  I will do eventually!

---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,19 @@
+
+
+public class PowerUps 
+{
+    public string Name;
+    public int Cost;
+    public Sprite Icon;
+    public int Quantity;
+
+    public PowerUps(string name, int cost, Sprite icon)
+    {
+        Name = name; 
+        Cost = cost;
+        Icon = icon;
+        Quantity = 0;
+    }
+
+    public virtual void ApplyEffect(GameObject player) { }
+}
---
+++ Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,11 @@
+
+
+public class SpeedBoost : PowerUps
+{
+    public SpeedBoost(Sprite icon) : base("Speed Boost", 50, icon) { }
+
+    public override void ApplyEffect(GameObject player)
+    {
+        player.GetComponent<PlayerController>().moveSpeed += 2f;
+    }
+}

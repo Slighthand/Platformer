1df7909d8420360b6edb6b7687106e203d2e3746
Slighthand <adeline.siu251@ycdsbk12.ca>
2025-06-10 23:23:59 -0400

    implemented bubble sort

--- Assets/Scripts/Collectable/Key.cs
+++ Assets/Scripts/Collectable/Key.cs
@@ -6,11 +6,15 @@ public class Key : MonoBehaviour
 {
     [SerializeField]
     private static int _keyCount;
+    private string[] color;
     [SerializeField]
     private string colour;
     public GameObject door;
     private bool doorDestroyed;
-
+    private void Start()
+    {
+        string[] colour = new string[3];
+    }
     public void OnTriggerEnter(Collider other)
     {
         PlayerInventory playerInventory = other.GetComponentInParent<PlayerInventory>(); // check if collison is with char
@@ -30,6 +34,27 @@ public class Key : MonoBehaviour
         {
             doorDestroyed = true;
             Destroy(door);
+            //print ("Congrats! You've collected the {BubbleSort(colour)} keys");
+        }
+    }
+
+    public static void BubbleSort(string[] keys)
+    {
+        for (int j = 0; j < keys.Length; j++)
+        {
+            bool swapped = false;
+            for (int i = 0; i < keys.Length - 1; i++)
+            {
+                if (string.Compare(keys[i], keys[i + 1]) > 0)
+                {
+                    string tmp = keys[i];
+                    keys[i] = keys[i + 1];
+                    keys[i + 1] = tmp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
         }
     }
 }

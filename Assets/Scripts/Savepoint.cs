875bfecac02942dc2f865bbd8f0308e46b0e340e
anqiey <144811036+anqiey@users.noreply.github.com>
2025-06-11 10:58:21 -0400

    Second attempt at resolving errors

--- Assets/Scripts/Savepoint.cs
+++ Assets/Scripts/Savepoint.cs
@@ -2,14 +2,14 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-[Serializable] public class Savepoint {
-    public Area Area;
-    public int Index;
-    public Vector3 Position;
+// [Serializable] public class Savepoint {
+//     public Area Area;
+//     public int Index;
+//     public Vector3 Position;
     
-    public Savepoint(Area area, int index, Vector3 position) {
-        Area = area;
-        Index = index;
-        Position = position;
-    }
-}
+//     public Savepoint(Area area, int index, Vector3 position) {
+//         Area = area;
+//         Index = index;
+//         Position = position;
+//     }
+// }

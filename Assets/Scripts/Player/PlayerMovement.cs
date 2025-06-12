0b3afcfcc048971bca87ea528842f3d9fcc0b71d
Tarlan <158780016+ajidf@users.noreply.github.com>
2025-06-11 21:40:53 -0400

    Finished the ShopMarket!

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,65 @@
 using UnityEngine;
 using static Wabubby.Extensions;
 
-[RequireComponent (typeof (Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
     [SerializeField, Range(0f, 30f)] public float speed = 6;
 
     bool stopControl = false;
     Rigidbody2D physics;
     PlayerInputCoordinator input;
 
-    void Start() {
+     //Shield-related
+    private bool shieldActive = false;
+    private float shieldTimer = 0f;
+    [SerializeField] private float shieldDuration = 5f;
+
+    void Start()
+    {
         input = transform.root.GetComponent<PlayerInputCoordinator>();
         physics = GetComponent<Rigidbody2D>();
     }
 
-    protected void OnDisable() {
+    protected void OnDisable()
+    {
         stopControl = false;
     }
 
-
-	protected void Update() {
+    void Update()
+    {
         physics.velocity = speed * input.Movement;
-	}
 
+        
+        if (shieldActive)
+        {
+            shieldTimer -= Time.deltaTime;
+            if (shieldTimer <= 0)
+            {
+                DeactivateShield();
+            }
+        }
+    }
+
+    
+    public void ActivateShield()
+    {
+        shieldActive = true;
+        shieldTimer = shieldDuration;
+        Debug.Log("Shield Activated!");
+       
+    }
+
+    private void DeactivateShield()
+    {
+        shieldActive = false;
+        Debug.Log("Shield Deactivated!");
+       
+    }
+
+    
+    public bool IsShieldActive()
+    {
+        return shieldActive;
+    }
 }

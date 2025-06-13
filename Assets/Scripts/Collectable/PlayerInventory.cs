49cff44a004481cbbf11d9397bed6e28bc2ce2ba
Slighthand <adeline.siu251@ycdsbk12.ca>
2025-06-13 02:25:04 -0400

    Made portal script

--- Assets/Scripts/Collectable/PlayerInventory.cs
+++ Assets/Scripts/Collectable/PlayerInventory.cs
@@ -12,7 +12,6 @@ public class PlayerInventory : MonoBehaviour
 
     [Header("Keys")]
     public Key key;
-    private bool _keyCollected;
     public string[] Keys = new string[6];
     public int Count = 0;
 
@@ -26,7 +25,6 @@ public class PlayerInventory : MonoBehaviour
         if (other.gameObject.CompareTag("Key"))
         {
             Debug.Log("Key collected");
-            _keyCollected = true;
             AddKey(key);
             Destroy(other.gameObject);
         }
